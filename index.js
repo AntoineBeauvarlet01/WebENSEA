0707a91866cb80128caa4d0ed6d632e56e48e947
AntoineBeauvarlet01 <antoine.beauvarlet@ensea.fr>
2024-10-24 15:04:16 +0200

    Ajout Users

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 app.use(express.json());
+app.listen(port, () => {console.log(`Serveur en cours d'exécution sur http://localhost:${port}`);});
 
 app.get("/", (req, res) => {
     res.json({
@@ -12,6 +13,14 @@ app.get("/", (req, res) => {
     })
 })
 
-app.listen(port, () => {
-	console.log(`Serveur en cours d'exécution sur http://localhost:${port}`);
-});
+const users = [
+	{ id: 1, firstName: 'John', lastName: 'Doe', role: 'admin' },
+	{ id: 2, firstName: 'Jane', lastName: 'Smith', role: 'user' },
+	{ id: 3, firstName: 'Alice', lastName: 'Johnson', role: 'moderator' },
+	{ id: 4, firstName: 'Bob', lastName: 'Brown', role: 'user' },
+	{ id: 5, firstName: 'Charlie', lastName: 'Davis', role: 'admin' }
+];
+// GET : LIRE tous les utilisateurs
+app.get("/users", (req, res) => {
+	res.json(users)
+})
