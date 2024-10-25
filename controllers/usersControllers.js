6e6d453e58b76f8b7af4760793eb8787d4bd4cc4
AntoineBeauvarlet01 <antoine.beauvarlet@ensea.fr>
2024-10-25 10:20:47 +0200

    POST OK

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,4 +1,5 @@
 const db = require("../database")
+
 exports.getAllUsers = function (req, res) {
 	db.all("SELECT * FROM users", [], (err, rows) => {
 		if (err) {
@@ -7,4 +8,44 @@ exports.getAllUsers = function (req, res) {
 			res.json(rows)
 		}
 	})
+}
+
+
+exports.createNewUser = function (req, res)  {
+    try {
+        const {  firstName, lastName } = req.body;
+
+
+        // regex pour alphanumérique seulement
+        function isAlphanumeric(str) {
+            const regex = /^[a-zA-Z0-9]+$/
+            console.log(regex.test(str))
+            return regex.test(str)
+        }
+
+
+        // Validate user data (optional but recommended)
+        if ( !firstName || !lastName  ) 
+            return res.status(400).json({ error: "Le prénom et le nom de famille sont requis !" });
+        if ( firstName === "Claire")
+            return res.status(400).json({ error: "Weird name" });
+        if (!isAlphanumeric(firstName))
+            return res.status(400).json({ error: "Ce nom n'est pas autorisé !" })
+    
+        db.run(
+            "INSERT INTO users (firstName, lastName) VALUES (?, ?)",
+            [firstName, lastName],
+            function (err) {
+                if (err) {
+                    res.status(500).json({ error: err.message })
+                } else {
+                    res.status(201).json({ id: this.lastID, firstName })
+                }
+            }
+        )
+      
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal server error" });
+    }
 }
